0416462ded300c716c1d96e4d160181877fcc5af
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2023-03-31 16:43:26 -0500

    installed material icons and confirmed it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import AbcIcon from '@mui/icons-material/Abc';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <p>Hello World</p>
       <Button variant="contained">Hello World</Button>
+      <AbcIcon fontSize='large' color='primary' />
     </div>
   );
 }
