96f4bfe4c1cc222c92377bc1cf3858505897554e
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2023-03-31 16:37:15 -0500

    installed material ui and confirmed it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
