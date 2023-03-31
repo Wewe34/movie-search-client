453d35c0bfbafc35d535a9a43f42a05086055e19
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2023-03-31 18:20:42 -0500

    Navbar items and alignment

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { 
+    AppBar, 
+    Grid, 
+    Button, 
+    Typography, 
+    Toolbar, 
+    Box,
+    TextField,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+
 
 function NavBar() {
-
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar sx={{display: 'flex'}}>
+                    <Box sx={{display: 'flex'}}>
+                        <MenuIcon
+                            fontSize="large"
+                            sx={{ mr: 2 }}
+                        >
+                        </MenuIcon>
+                        <Typography variant="h6" component="div" sx={{mr: 2, minWidth: '30px'}} >
+                            Flixim
+                        </Typography>
+                    </Box>
+                        <TextField 
+                            variant="filled"
+                            placeholder="Search Movies"
+                            sx={{flexGrow: 1, mx: 2}}
+                        />
+                        <Button color="inherit">Sign In</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
 }
 
 export default NavBar;
