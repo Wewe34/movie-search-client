f95e2254569147d0a71290d0889e6a901ce2145a
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2023-03-31 20:22:29 -0500

    mobile search bar visible when Search Icon is clicked, added a cancel icon to hide mobile search bar when clicked

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { 
     AppBar, 
-    Grid, 
     Button, 
     Typography, 
     Toolbar, 
     Box,
     TextField,
     useMediaQuery,
-    createTheme,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import CancelIcon from '@mui/icons-material/Cancel';
 import {accent, dark } from '../styles';
 
 
 
 function NavBar() {
+    const [showSmallDeviceSearch, setShowSmallDeviceSearch] = useState(false);
+
     const smallDevice = useMediaQuery("(max-width:600px)");
     return (
         <Box sx={{ flexGrow: 1}}>
             <AppBar position="static" sx={{ backgroundColor: `${dark}` }}>
+                {showSmallDeviceSearch && smallDevice ? 
+                <Box sx={{display: 'flex'}}>
+                    <TextField
+                        placeholder="Search Flixim"
+                        sx={{flexGrow: 4, mx: 2, my: 2, backgroundColor: 'white', borderRadius: 2, minWidth:'200px'}}/> 
+                    <CancelIcon onClick={() => setShowSmallDeviceSearch(false)} fontSize='large' sx={{flexGrow: 2, alignSelf: 'center'}} />
+                </Box> :
                 <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
                     <Box sx={{display: 'flex', flexGrow: 1}}>
                         <MenuIcon
@@ -34,14 +42,14 @@ function NavBar() {
                     </Box>
                     <Box sx={{display: 'flex',flexGrow: 3}}>
                     {smallDevice ? 
-                    <SearchIcon sx={{flexGrow: 3, alignSelf: 'center'}} /> :
+                        <SearchIcon onClick={() => setShowSmallDeviceSearch(true)} sx={{my: 4, flexGrow: 3, alignSelf: 'center'}} /> :
                         <TextField
                             placeholder="Search Flixim"
                             sx={{flexGrow: 3, mx: 2, my: 2, backgroundColor: 'white', borderRadius: 2, minWidth:'200px'}}
                         />}
                      <Button sx={{flexGrow: 1, color: 'whitesmoke'}}>Sign In</Button>   
                     </Box>
-                </Toolbar>
+                </Toolbar>}
             </AppBar>
         </Box>
     )
