5d26836c8250800654a64bf686bf5b0b7d3108cb
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2023-04-01 23:19:36 -0500

    routes for favorites and watchlist

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,23 @@
+import { RouteSharp } from '@mui/icons-material';
 import React from 'react';
 import {
     Route,
-    Redirect,
-    Switch
-} from 'react-router-dom';
+    redirect, Routes as Switch } from 'react-router-dom';
+import Favorites from './components/Favorites';
+import SearchResults from './components/SearchResults';
+import Watchlist from './components/Watchlist';
 
+
+  
 function Routes() {
+
     return (
         <Switch>
-            
+            <Route path='/' element={<SearchResults/>}/>
+            <Route path='/favorites' element={<Favorites/>}/>
+            <Route path='/watchlist' element={<Watchlist/>}/>
         </Switch>
     )
 }
+
+export default Routes;
