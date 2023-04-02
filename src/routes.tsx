597da110230b00826e0c68f8a1245a56a082a9e9
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2023-04-01 22:41:40 -0500

    installed react router dom

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch
+} from 'react-router-dom';
+
+function Routes() {
+    return (
+        <Switch>
+            
+        </Switch>
+    )
+}
