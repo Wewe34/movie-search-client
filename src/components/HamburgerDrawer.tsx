1a35f634ffbe733b0abb49be8ede48210c84827e
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2023-04-01 22:34:18 -0500

    hamburger drawer options added with icons

--- src/components/HamburgerDrawer.tsx
+++ src/components/HamburgerDrawer.tsx
@@ -1,5 +1,8 @@
-import { Box, Drawer } from "@mui/material";
 import React, { Dispatch, SetStateAction, useState } from "react";
+import { Box, Divider, Drawer, Typography } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
+import PersonIcon from '@mui/icons-material/Person';
 
 
 interface IHamburgerDrawerProps {
@@ -17,8 +20,22 @@ function HamburgerDrawer(props: IHamburgerDrawerProps) {
                 anchor="left"
                 open={isOpen}
                 onClose={() => closeDrawer(false)}
+                sx={{minWidth:'50%'}}
                 >
-                Hello
+                <Box sx={{paddingRight:7, paddingLeft:3, paddingY:3}}>
+                    <Box sx={{display:"flex"}}>
+                        <FavoriteIcon />
+                        <Typography  sx={{paddingBottom:5, paddingLeft:2}}>My Favorites</Typography>
+                    </Box>
+                    <Box sx={{display:"flex"}}>
+                        <BookmarkOutlinedIcon/>
+                        <Typography  sx={{paddingBottom:5, paddingLeft:2}}>My Watchlist</Typography>
+                    </Box>
+                    <Box sx={{display:"flex"}}>
+                        <PersonIcon />
+                        <Typography  sx={{paddingBottom:5, paddingLeft:2}}>Sign In</Typography>
+                    </Box>
+                </Box>
             </Drawer>
         </>
     )
