1ac2a79fe79f6ad7c9bf0fa8c9d6308f23e6b874
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2023-04-01 23:54:29 -0500

    clear recently viewed history complete

--- src/components/RecentlyViewed.tsx
+++ src/components/RecentlyViewed.tsx
@@ -2,17 +2,17 @@ import { Box, Typography } from "@mui/material";
 import React, { useState } from "react";
 import {IResults} from './SearchResults';
 import {mockData} from '../mockData';
-import { blue } from "@mui/material/colors";
+import { blue, grey } from "@mui/material/colors";
 
 function RecentlyViewed() {
 
     const [recentlyViewed, setRecentlyViewed] = useState<IResults[]>(mockData.slice(0,3));
 
     return (
-        <Box sx={{display: 'flex', flexDirection: 'column', maxWidth: '100%', paddingX: 1, backgroundColor:"whitesmoke"}}>
+        <Box sx={{display: 'flex', flexDirection: 'column', maxWidth: '100%', paddingX: 1, paddingBottom: 4,backgroundColor: 'black', color:'whitesmoke'}}>
             <Box sx={{display:"flex", justifyContent:"space-between", flexWrap:"wrap"}}>
                 <Typography variant="h4" sx={{paddingTop:3, paddingLeft:2}} >Recently Viewed</Typography>
-                <Typography variant="body1" sx={{padding:2, color: blue[500]}} >Clear History</Typography>
+                <Typography variant="body1" sx={{padding:2, color: blue[500]}} onClick={() => setRecentlyViewed([])} >Clear History</Typography>
             </Box>
             {recentlyViewed.length ? 
                <Box sx={{display: 'flex', overflowX:'auto'}}>
@@ -27,7 +27,7 @@ function RecentlyViewed() {
                                 />
                             </Box>
                 })}
-            </Box> : 'You have no recently viewed pages'}
+            </Box> : <Typography variant="body1" sx={{paddingY:7, paddingLeft:2}} >You have no recently viewed movies</Typography> }
         </Box>   
     )
 }
